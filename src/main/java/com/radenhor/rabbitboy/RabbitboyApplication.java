c77d92fcda0df1d17dcdb0f12f329288d34a03c1
radenhor <radenhor9@gmail.com>
2021-03-30 10:59:40 +0700

    init project

---
+++ src/main/java/com/radenhor/rabbitboy/RabbitboyApplication.java
@@ -0,0 +1,18 @@
+package com.radenhor.rabbitboy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class RabbitboyApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitboyApplication.class, args);
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(RabbitboyApplication.class);
+    }
+}
---
+++ src/test/java/com/radenhor/rabbitboy/RabbitboyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.radenhor.rabbitboy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitboyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
